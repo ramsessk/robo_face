430118b69b687d96fe8a25043d9398cf88be3e1e
Steven <ramsessk@gmail.com>
2026-04-12 20:49:08 +0900

    모바일 입처럼 보이기

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+            <meta name="apple-mobile-web-app-capable" content="yes">
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
             <title>Robo Client Final</title>
             <style>
                 body { 
